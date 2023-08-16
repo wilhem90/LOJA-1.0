8fdfe1ab49780e1f499706411a9475e8d0e6f694
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2023-08-15 21:06:40 -0300

    refresh

---
+++ assets/scripts/index.js
@@ -0,0 +1,12 @@
+const d = document,
+main = d.querySelector('main')
+main.innerHTML = 'Bon si misye kite bòlèt, nou mèt femen bank sa!'
+
+main.style.fontWeight = 'bolder'
+main.style.textTransform = 'uppercase'
+main.style.border = '1px solid black'
+main.style.display = 'flex'
+main.style.flexDirection = 'column'
+main.style.justifyContent = 'space-between'
+main.style.alignItems = ''
+
