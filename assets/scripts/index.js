37ef23a1663825805695980771a0b8ac3e0ab802
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2023-08-15 21:32:15 -0300

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,30 @@
-const d = document,
-main = d.querySelector('main')
-main.innerHTML = 'Bon si misye kite bòlèt, nou mèt femen bank sa!'
+const d = document
+const main = d.querySelector('main')
 
-main.style.fontWeight = 'bolder'
-main.style.textTransform = 'uppercase'
-main.style.border = '1px solid black'
-main.style.display = 'flex'
-main.style.flexDirection = 'column'
-main.style.justifyContent = 'space-between'
-main.style.alignItems = ''
+const getHTML = (options) => {
+   let {url, success, error} = options; 
+   const xhr = new XMLHttpRequest()
+   xhr.addEventListener('readystatechange', (e) => {
+    if (xhr.readyState === 4) return;
 
+    if (xhr.status === 200 && xhr.status < 300) {
+        let html = xhr.responseText;
+        success(html);
+    }else {
+        let message = xhr.statusText || 'Aconteceu uma coisa errada'
+        error(`Error: ${xhr.status}: ${message}`);
+    }
+   })
+
+   xhr.open('GET', url)
+   xhr.setRequestHeader("Content-type", "text/html; charset=utf-8")
+   xhr.send();
+}
+
+d.addEventListener('DOMContentLoaded', (e) => {
+    getHTML({
+        url: "/assets/paginas/home.html",
+        success:(html) => main.innerHTML = html,
+        error:(error) => main.innerHTML = `<h1>${error}</h1`
+    })
+})
